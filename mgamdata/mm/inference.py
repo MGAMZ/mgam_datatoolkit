ef7d4b43dc07a22dd911b3badbf92611e14fde03
MGAM <312065559@qq.com>
2024-09-27 10:17:22 +0000

    develop for evaluation functions

--- mgamdata/mm/inference.py
+++ mgamdata/mm/inference.py
@@ -3,7 +3,7 @@ import os.path as osp
 import pdb
 import warnings
 from tqdm import tqdm
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Generator
 from pprint import pprint
 from colorama import Fore, Style
 
@@ -19,15 +19,7 @@ from ..io.sitk_toolkit import LoadDcmAsSitkImage
 
 
 
-
 INFERENCER_WORK_DIR = "/fileser51/zhangyiqin.sx/mmseg/work_dirs_inferencer/"
-
-
-
-def dice_coeff(pred, target):
-    eps = 1e-7
-    intersection = (pred * target).sum()
-    return (2. * intersection + eps) / (pred.sum() + target.sum() + eps)
 
 
 
@@ -46,31 +38,59 @@ def visualize(image_array, gt_class_idx, pred_class_idx):
 class Inferencer_2D:
     def __init__(self, cfg_path, ckpt_path):
         self.model:BaseSegmentor = init_model(cfg_path, ckpt_path)
-    
+        self.model.eval()
+        self.model.requires_grad_(False)
+
     @torch.inference_mode()
-    def Inference_FromDcm(self, dcm_slide_folder:str, spacing=None
-            ) -> Tuple[sitk.Image, sitk.Image]:
-        image, _, _, _ = LoadDcmAsSitkImage('engineering', dcm_slide_folder, spacing=spacing)
-        image_array = sitk.GetArrayFromImage(image)
+    def Inference_FromNDArray(self, image_array):
         image_array = [i for i in image_array]
         data, is_batch = _preprare_data(image_array, self.model)
-        
+
         # forward the model
         results = []
         data = self.model.data_preprocessor(data, False)
         inputs = torch.stack(data['inputs'])
         data_samples = [sample.to_dict() for sample in data['data_samples']]
         for array, sample in tqdm(zip(inputs, data_samples),
-                                  desc="Inference",
+                                  desc="Inferencing",
                                   total=len(inputs),
                                   dynamic_ncols=True,
-                                  leave=False):
+                                  leave=False,
+                                  mininterval=1):
             result = self.model.inference(array[None], [sample])
             results.append(result)
-        
+
+        pred = torch.cat(results, axis=0).transpose(0,1) # [Class, D, H, W]
+        return pred
+
+
+    def Inference_FromITK(self, itk_image:sitk.Image) -> Tuple[sitk.Image, sitk.Image]:
+        image_array = sitk.GetArrayFromImage(itk_image) # [D, H, W]
+        pred = self.Inference_FromNDArray(image_array) # [Class, D, H, W]
         # 后处理
-        pred = torch.cat(results, axis=0).transpose(0,1)   # [Class, D, H, W]
-        pred = pred.argmax(axis=0).to(dtype=torch.uint8, device='cpu').numpy()
-        pred = sitk.GetImageFromArray(pred)
-        pred.CopyInformation(image)
-        return image, pred
+        pred = pred.argmax(axis=0).to(dtype=torch.uint8, device='cpu').numpy() # [D, H, W]
+        itk_pred = sitk.GetImageFromArray(pred)
+        itk_pred.CopyInformation(itk_image)
+        return itk_image, itk_pred
+
+
+    def Inference_FromDcm(self, dcm_slide_folder:str, spacing=None):
+        image, _, _, _ = LoadDcmAsSitkImage('engineering', dcm_slide_folder, spacing=spacing)
+        return self.Inference_FromITK(image)
+
+
+    def Inference_FromITKFolder(self, folder:str):
+        mha_files = []
+        for root, _, files in os.walk(folder):
+            for file in files:
+                if file.endswith('.mha'):
+                    mha_files.append(osp.join(root, file))
+        print(f"Inferencing from Folder: {folder}.")
+        
+        for mha_path in tqdm(mha_files,
+                             desc='Inference_FromITKFolder',
+                             leave=False,
+                             dynamic_ncols=True):
+            itk_image = sitk.ReadImage(mha_path)
+            itk_image, itk_pred = self.Inference_FromITK(itk_image)
+            yield itk_image, itk_pred, mha_path
--- mgamdata/utils/evaluation_mha.py
+++ mgamdata/utils/evaluation_mha.py
@@ -6,16 +6,42 @@ import argparse
 import os
 import os.path as osp
 import pdb
+from pathlib import Path
 from multiprocessing import Pool
 from tqdm import tqdm
 from pprint import pprint
 
 import torch
+import pandas
 import numpy as np
 import SimpleITK as sitk
 
 from mmseg.models.losses.dice_loss import dice_loss
 
+
+
+
+def calc_dice(gt_data:np.ndarray, pred_data:np.ndarray):
+    gt_class = torch.from_numpy(gt_data).cuda()
+    pred_class = torch.from_numpy(pred_data).cuda()
+    dice = 1 - dice_loss(gt_class[None], pred_class[None], weight=None, ignore_index=None
+                            ).cpu().numpy()
+    return dice
+
+
+
+def calc_area_metric(gt_data:np.ndarray, pred_data:np.ndarray):
+    # 计算iou, recall, precision
+    gt_class = torch.from_numpy(gt_data).cuda()
+    pred_class = torch.from_numpy(pred_data).cuda()
+    tp = (gt_class * pred_class).sum()
+    fn = gt_class.sum() - tp
+    fp = pred_class.sum() - tp
+    
+    iou = (tp / (tp + fn + fp)).cpu().numpy()
+    recall = (tp / (tp + fn)).cpu().numpy()
+    precision = (tp / (tp + fp)).cpu().numpy()
+    return iou, recall, precision
 
 
 
@@ -30,85 +56,73 @@ def calculate_one_pair(gt_path:str, pred_path:str, only_L3:bool=False, invert:bo
     Returns:
         _type_: _description_
     """
+
+    gt = sitk.ReadImage(gt_path)
+    pred = sitk.ReadImage(pred_path)
     
-    try:
-        gt = sitk.ReadImage(gt_path)
-        pred = sitk.ReadImage(pred_path)
-        
-        gt_data = sitk.GetArrayFromImage(gt)
-        pred_data = sitk.GetArrayFromImage(pred)
-        if invert:
-            pred_data = pred_data[::-1].copy()
-        if gt_data.shape != pred_data.shape:
-            return gt_data.shape, pred_data.shape, gt_path, pred_path
-        
-        # 如果只计算L3的dice, 则在[D, H, W]的D维度上
-        # 寻找gt的any有效slice
-        # 并相应抛弃其他所有slices
-        if only_L3:
-            valid_slices = gt_data.any(axis=(1, 2))
-            gt_data = gt_data[valid_slices][3:]
-            pred_data = pred_data[valid_slices][3:]
-        
-        # 生成one-hot编码对
-        gt_data = np.stack([gt_data == i for i in range(1, 5)], axis=0)
-        pred_data = np.stack([pred_data == i for i in range(1, 5)], axis=0)
-        
-        # 逐类计算dice
-        dices = []
-        for i in range(4):
-            gt_class = torch.from_numpy(gt_data[i]).cuda()
-            pred_class = torch.from_numpy(pred_data[i]).cuda()
-            dice = 1 - dice_loss(gt_class[None], pred_class[None], weight=None, ignore_index=None
-                                 ).cpu().numpy()
-            dices.append(dice)
-        
-        return np.stack(dices)
+    gt_data = sitk.GetArrayFromImage(gt)
+    pred_data = sitk.GetArrayFromImage(pred)
+    if invert:
+        pred_data = pred_data[::-1].copy()
+    if gt_data.shape != pred_data.shape:
+        return gt_data.shape, pred_data.shape, gt_path, pred_path
     
-    except Exception as e:
-        return f"Failed gt: {gt_path} | pred: {pred_path} | reason: {e}"
+    # 如果只计算L3的dice, 则在[D, H, W]的D维度上
+    # 寻找gt的any有效slice
+    # 并相应抛弃其他所有slices
+    if only_L3:
+        valid_slices = gt_data.any(axis=(1, 2))
+        gt_data = gt_data[valid_slices][3:]
+        pred_data = pred_data[valid_slices][3:]
+    
+    # 生成one-hot编码对
+    gt_data = np.stack([gt_data == i for i in range(1, 5)], axis=0)
+    pred_data = np.stack([pred_data == i for i in range(1, 5)], axis=0)
+    
+    # 逐类计算metric
+    dices, ious, recalls, precisions = [], [], [], []
+    for i in range(4):
+        dice = calc_dice(gt_data[i], pred_data[i])
+        iou, recall, precision = calc_area_metric(
+            gt_data[i].astype(np.uint8), pred_data[i].astype(np.uint8))
+        
+        dices.append(dice)
+        ious.append(iou)
+        recalls.append(recall)
+        precisions.append(precision)
+    
+    # 每个指标包含四个值，代表四个类
+    return {
+        'seriesUID': Path(pred_path).stem,
+        'dice': np.stack(dices),
+        'iou': np.stack(ious),
+        'recall': np.stack(recalls),
+        'precision': np.stack(precisions)
+    }
 
 
 
-def evaluate_one_folder(gt_folder:str, pred_folder:str, only_L3:bool=False, use_mp:bool=False, invert:bool=False):
+def evaluate_one_folder(gt_folder:str, 
+                        pred_folder:str, 
+                        only_L3:bool=False, 
+                        use_mp:bool=False, 
+                        invert:bool=False):
     pred_files = sorted([file for file in os.listdir(pred_folder) if file.endswith('.mha')])
     gt_files = [file.replace(pred_folder, gt_folder) for file in pred_files]
     
-    dice_list = []
-    failed_list = []
-    if use_mp:
-        with Pool(32) as p:
-            results = []
-            for pred_file in tqdm(pred_files,
-                                  desc='Evaulating',
-                                  dynamic_ncols=True,
-                                  leave=False):
-                gt_path = osp.join(gt_folder, pred_file)
-                pred_path = osp.join(pred_folder, pred_file)
-                one_task = p.apply_async(calculate_one_pair, (gt_path, pred_path, only_L3, invert))
-                results.append(one_task)
-            
-            for result in results:
-                out = result.get()
-                if isinstance(out, np.ndarray):
-                    dice_list.append(out)
-                else:
-                    failed_list.append(out)
-    else:
-        for gt_file, pred_file in tqdm(zip(gt_files, pred_files),
-                                        desc='Evaulating',
-                                        total=len(gt_files),
-                                        dynamic_ncols=True,
-                                        leave=False):
-            gt_path = osp.join(gt_folder, gt_file)
-            pred_path = osp.join(pred_folder, pred_file)
-            out = calculate_one_pair(gt_path, pred_path, only_L3, invert)
-            if isinstance(out, np.ndarray):
-                dice_list.append(out)
-            else:
-                failed_list.append(out)
+    metric_list = []
     
-    return dice_list, failed_list
+    for gt_file, pred_file in tqdm(zip(gt_files, pred_files),
+                                    desc='Evaulating',
+                                    total=len(gt_files),
+                                    dynamic_ncols=True,
+                                    leave=False):
+        gt_path = osp.join(gt_folder, gt_file)
+        pred_path = osp.join(pred_folder, pred_file)
+        out = calculate_one_pair(gt_path, pred_path, only_L3, invert)
+        metric_list.append(out)
+    
+    return metric_list
 
 
 
@@ -127,7 +141,7 @@ def parser_args():
 
 if __name__ == '__main__':
     args = parser_args()
-    dice_list, failed_list = evaluate_one_folder(args.gt_root, args.pred_root, only_L3=args.whole_series, invert=args.invert)
-    mean_dice = np.mean(dice_list, axis=0)
-    pprint(mean_dice)
-    pprint(failed_list)
+    metric_list = evaluate_one_folder(args.gt_root, args.pred_root, only_L3=args.whole_series, invert=args.invert)
+    # metric_list中元素的数量是样本数量
+    # 每个元素是一个字典，包含四个指标的四个类的值和自身的SeriesUID
+    result = pandas.DataFrame(metric_list)
