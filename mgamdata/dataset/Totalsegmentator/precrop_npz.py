db499ec97fcc09db92410493f3a2af6323e2ffbd
mgam <312065559@qq.com>
2024-10-22 22:56:31 +0800

    develop precrop util

---
+++ mgamdata/dataset/Totalsegmentator/precrop_npz.py
@@ -0,0 +1,72 @@
+import os
+import argparse
+import multiprocessing as mp
+from tqdm import tqdm
+
+import numpy as np
+import SimpleITK as sitk
+
+from mgamdata.mm.mmseg_Dev3D import RandomCrop3D
+
+
+def crop_per_series(args:tuple):
+    cropper, series_path, num_cropped, save_path = args
+    cropper: RandomCrop3D
+    series_path: str
+    num_cropped: int
+    save_path: str
+    
+    image_itk_image = sitk.ReadImage(os.path.join(series_path, 'ct.mha'))
+    anno_itk_image = sitk.ReadImage(os.path.join(series_path, 'segmentations.mha'))
+    image_array = sitk.GetArrayFromImage(image_itk_image)
+    anno_array = sitk.GetArrayFromImage(anno_itk_image)
+    data = {
+        'img': image_array,
+        'gt_sem_seg': anno_array,
+        'seg_fields': ['gt_sem_seg'],
+    }
+    
+    os.makedirs(save_path, exist_ok=True)
+    for crop_idx in range(num_cropped):
+        crop_bbox = cropper.crop_bbox(data)
+        cropped_image_array = cropper.crop(image_array, crop_bbox).astype(np.int16)
+        cropped_anno_array = cropper.crop(anno_array, crop_bbox).astype(np.uint8)
+        np.savez_compressed(
+            file=os.path.join(save_path, f'{os.path.basename(save_path)}_{crop_idx}.npz'),
+            img=cropped_image_array,
+            gt_sem_seg=cropped_anno_array)
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser('Pre-Random-Crop 3D')
+    argparser.add_argument('source_mha_folder', type=str, help='The folder containing mha files.')
+    argparser.add_argument('dest_npz_folder', type=str, help='The folder to save npz files.')
+    argparser.add_argument('crop_size', type=int, nargs=3, help='The size of cropped volume.')
+    argparser.add_argument('crop_cat_max', type=float, default=0.9, help='Max ratio for single catagory can occupy.')
+    argparser.add_argument('num_cropped', type=int, help='The number of cropped volumes per series.')
+    argparser.add_argument('--ignore-index', type=int, default=0, help='The index to ignore in segmentation.')
+    argparser.add_argument('--mp', action='store_true', default=False, help='Whether to use multiprocessing.')
+    args = argparser.parse_args()
+    
+    task_list = []
+    for series in os.listdir(args.source_mha_folder):
+        task_list.append((
+            RandomCrop3D(args.crop_size, args.crop_cat_max, args.ignore_index),
+            os.path.join(args.source_mha_folder, series),
+            args.num_cropped,
+            os.path.join(args.dest_npz_folder, series)))
+    
+    results = []
+    if args.mp:
+        with mp.Pool() as pool:
+            fetcher = pool.imap_unordered(crop_per_series, task_list)
+            for result in tqdm(fetcher, "Cropping", total=len(task_list), 
+                               dynamic_ncols=True, leave=False):
+                results.append(result)
+    else:
+        for task in tqdm(task_list, "Cropping",
+                         dynamic_ncols=True, leave=False):
+            result = crop_per_series(task)
+            results.append(result)
+    
+    print(f"Finished cropping {len(results)} series.")
