ff109f1ae6465cc3fdb46ae2c7575bd0f2979290
MGAM <312065559@qq.com>
2024-11-29 23:20:03 +0800

    introduce LUNA16 Dataset

--- mgamdata/dataset/LUNA16/mm_dataset.py
+++ mgamdata/dataset/LUNA16/mm_dataset.py
@@ -3,13 +3,13 @@ from .meta import CLASS_INDEX_MAP
 
 
 
-class KiTS23_base:
+class LUNA16_base:
     METAINFO = dict(classes=list(CLASS_INDEX_MAP.keys()))
 
 
-class KiTS23_Precrop_Npz(KiTS23_base, mgam_Standard_Precropped_Npz):
+class LUNA16_Precrop_Npz(LUNA16_base, mgam_Standard_Precropped_Npz):
     pass
 
 
-class KiTS23_Mha(KiTS23_base, mgam_Standard_3D_Mha):
+class LUNA16_Mha(LUNA16_base, mgam_Standard_3D_Mha):
     pass
