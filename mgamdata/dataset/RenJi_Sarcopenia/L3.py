67c9c1de6a9d779ab9c6d3cb6a85862aa6e1526d
MGAM <312065559@qq.com>
2025-03-21 02:27:27 +0000

    Fix a bad behavior from L3 locating function. This function may requires further improvements in the future.

--- mgamdata/dataset/RenJi_Sarcopenia/L3.py
+++ mgamdata/dataset/RenJi_Sarcopenia/L3.py
@@ -1,5 +1,4 @@
 import pdb
-import warnings
 
 import numpy as np
 import pandas as pd
@@ -15,7 +14,7 @@ def find_L3_slices(seriesUIDs: list[str]|str, L3_df:pd.DataFrame|None=None):
     for seriesUID in seriesUIDs:
         series_anno = L3_df.loc[L3_df['序列编号'] == seriesUID]
         series_L3 = series_anno[['L3节段起始层数', 'L3节段终止层数']].values.flatten()
-        if np.isnan(series_L3).any():
+        if np.isnan(series_L3).any() or len(series_L3) == 0:
             L3_slicess.append(None)
         else:
             L3_slicess.append(series_L3.astype(np.uint32))
