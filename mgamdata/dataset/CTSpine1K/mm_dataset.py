d48422f89711b459173684b1ada65926b64fa1f8
MGAM <312065559@qq.com>
2025-03-31 10:10:30 +0000

    Add CTSpine1K training dataset support.

---
+++ mgamdata/dataset/CTSpine1K/mm_dataset.py
@@ -0,0 +1,15 @@
+from ..base import mgam_SemiSup_3D_Mha, mgam_SemiSup_Precropped_Npz
+from .meta import CLASS_INDEX_MAP
+
+
+
+class CTSpine1K_base:
+    METAINFO = dict(classes=list(CLASS_INDEX_MAP.keys()))
+
+
+class CTSpine1K_Precrop_Npz(CTSpine1K_base, mgam_SemiSup_Precropped_Npz):
+    pass
+
+
+class CTSpine1K_Mha(CTSpine1K_base, mgam_SemiSup_3D_Mha):
+    pass
