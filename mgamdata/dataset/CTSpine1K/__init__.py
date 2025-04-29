a459e6fa942aecd8ca2a2755994a85ec9eaca81f
MGAM <312065559@qq.com>
2025-04-29 16:06:38 +0000

    sync

---
+++ mgamdata/dataset/CTSpine1K/__init__.py
@@ -0,0 +1,8 @@
+from .mm_dataset import CLASS_INDEX_MAP as CTSpine1K_CLASS_INDEX_MAP
+from .mm_dataset import CTSpine1K_Mha, CTSpine1K_Precrop_Npz
+
+__all__ = [
+    'CTSpine1K_CLASS_INDEX_MAP',
+    'CTSpine1K_Mha',
+    'CTSpine1K_Precrop_Npz',
+]
