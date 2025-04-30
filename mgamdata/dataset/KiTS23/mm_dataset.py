ea8a6db27f40f9bd39430e504472f95dba901343
mgam <312065559@qq.com>
2025-04-30 21:02:49 +0800

    1. Update KiTS23 API to the latest standard.

--- mgamdata/dataset/KiTS23/mm_dataset.py
+++ mgamdata/dataset/KiTS23/mm_dataset.py
@@ -1,4 +1,4 @@
-from ..base import mgam_Standard_3D_Mha, mgam_Standard_Precropped_Npz
+from ..base import mgam_SemiSup_Precropped_Npz, mgam_SemiSup_3D_Mha
 from .meta import CLASS_INDEX_MAP
 
 
@@ -7,9 +7,9 @@ class KiTS23_base:
     METAINFO = dict(classes=list(CLASS_INDEX_MAP.keys()))
 
 
-class KiTS23_Precrop_Npz(KiTS23_base, mgam_Standard_Precropped_Npz):
+class KiTS23_Precrop_Npz(KiTS23_base, mgam_SemiSup_Precropped_Npz):
     pass
 
 
-class KiTS23_Mha(KiTS23_base, mgam_Standard_3D_Mha):
+class KiTS23_Mha(KiTS23_base, mgam_SemiSup_3D_Mha):
     pass
